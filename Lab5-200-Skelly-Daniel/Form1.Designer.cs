f006ae4a473ec74052f371d16e447848d3971985
Daniel Skelly <dskelly@siue.edu>
2018-04-29 16:56:45 -0500

    Add project files.

---
+++ Lab5-200-Skelly-Daniel/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Lab5_200_Skelly_Daniel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTaxIncome = new System.Windows.Forms.TextBox();
+            this.txtTaxOwned = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Taxable Income";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Income Tax Owned";
+            // 
+            // txtTaxIncome
+            // 
+            this.txtTaxIncome.Location = new System.Drawing.Point(128, 22);
+            this.txtTaxIncome.Name = "txtTaxIncome";
+            this.txtTaxIncome.Size = new System.Drawing.Size(77, 20);
+            this.txtTaxIncome.TabIndex = 0;
+            // 
+            // txtTaxOwned
+            // 
+            this.txtTaxOwned.Location = new System.Drawing.Point(128, 53);
+            this.txtTaxOwned.Name = "txtTaxOwned";
+            this.txtTaxOwned.ReadOnly = true;
+            this.txtTaxOwned.Size = new System.Drawing.Size(77, 20);
+            this.txtTaxOwned.TabIndex = 1;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(35, 92);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 0;
+            this.calcButton.Text = "&Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Location = new System.Drawing.Point(130, 92);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calcButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(226, 133);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.txtTaxOwned);
+            this.Controls.Add(this.txtTaxIncome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "C Sharp U Income Tax Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTaxIncome;
+        private System.Windows.Forms.TextBox txtTaxOwned;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Lab5-200-Skelly-Daniel/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_200_Skelly_Daniel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)       //Calculates Income Tax when user clicks "Calculate"
+        {
+            decimal income = Convert.ToDecimal(txtTaxIncome.Text);      //converts the tax income textbox from a string to a decimal
+            decimal tax = .0m;                                          //initializes the tax variable to 0
+
+
+
+            if (income <= 9225)                                         //calculates the tax owned based on the taxable income amount entered                                     
+                tax = (int)(income * .10m);
+            else if (income > 9225 && income <= 37450)
+                tax = 922.50m + (int)((income - 9225) * .15m);
+            else if (income > 37450 && income <= 90750)
+                tax = 5156.25m + (int)((income - 37450) * .25m);
+            else if (income > 90750 && income <= 189300)
+                tax = 18481.25m + (int)((income - 90750) * .28m);
+            else if (income > 189300 && income <= 411500)
+                tax = 46075.25m + (int)((income - 189300) * .33m);
+            else if (income > 411500 && income <= 413200)
+                tax = 119401.25m + (int)((income - 411500) * .35m);
+            else if (income > 413200)
+                tax = (int)(income * .396m);
+
+            txtTaxOwned.Text = tax.ToString("c");                     //converts the calculated tax owned to a string and formats it
+            txtTaxIncome.Focus();                                     //refers to the tax income object for number input                               
+
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)       //Closes the program when user clicks "Exit"
+        {
+            this.Close();
+        }
+    }
+}
